d8b35f814cb58c47bb838a29bb1c1545b932c4d1
rifalinaaa <rifalina_nandari_30rpl@student.smktelkom-mlg.sch.id>
2023-02-02 21:25:04 +0700

    first commit

---
+++ app/src/main/java/com/example/biodataprofile/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.biodataprofile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("Biodata Pacar Rifa");
+        }
+    }
+}
+
